ff1469cb1e05cb2e876c578e230d40bb7380af37
Haonan <wuhaonan@eluying.com>
2016-01-04 14:53:13 +0800

    Initial Commit

---
+++ AFNTop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNTop
+//
+//  Created by Haonan on 16/1/4.
+//  Copyright © 2016年 Haonan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AFNTopTests/AFNTopTests.m
@@ -0,0 +1,39 @@
+//
+//  AFNTopTests.m
+//  AFNTopTests
+//
+//  Created by Haonan on 16/1/4.
+//  Copyright © 2016年 Haonan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AFNTopTests : XCTestCase
+
+@end
+
+@implementation AFNTopTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
